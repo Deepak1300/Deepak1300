8d6d2dcd2bfb1cfb6f0b5ac1d6c0a84bbf82d31f
Deepak1300 <141770129+Deepak1300@users.noreply.github.com>
2023-08-09 00:24:46 +0530

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Deepak1300
+- 👀 I’m interested in codeing 
+- 🌱 I’m currently learning c
+- 💞️ I’m looking to collaborate on app development 
+- 📫 How to reach me deepak.m1302@gmail.com
+
+<!---
+Deepak1300/Deepak1300 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
